53e69998a127821ed432384a467d1cc8c0e4ab1c
Nux007 <007.nux@gmail.com>
2021-02-08 13:18:46 +0100

    Exo 186.

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 1. Créez une classe VODService et ajoutez lui les propriétés suivantes:
+ *    - name : string
+ *    - price : prix
+ *
+ * 2. Créez les getters et les setters associés à ces propriétés.
+ *
+ * 3. Fites en sorte de pouvoir chaîner le deux setters
+ *
+ * 4. Instancier un objet à l'aide de la classe créée, instanciez dans le fichier index.php
+ *
+ * 5. Utilisez le chaînage des méthodes pour mettre à jour le nom et le prix en une seule ligne.
+ */
